9412259758f429ebe3ddb6a52ef6498b9ae0c7cb
Patrick Sernetz <patrickse@users.noreply.github.com>
2025-05-27 21:09:49 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
             inherit version;
             src = ./caddy-src;
             runVend = true;
-            vendorHash = "sha256-c9A0LabGN8gIq2pL/WP8wst3y0+8f72K015QZrX9yq4=";
+            vendorHash = "sha256-wKf3Njq+a9H+dRyj0hMsTJHzrLADPEVjpjCC0n0YA2w=";
             # vendorHash = pkgs.lib.fakeHash;
 
             meta = {
